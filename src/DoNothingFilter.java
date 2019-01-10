2f367dc339bd69aaa1b4c881bfc82b3639cb83b0
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2019-01-09 19:44:55 -0800

    update DoNothingFilter to reflect new PixelFilter interface

--- src/DoNothingFilter.java
+++ src/DoNothingFilter.java
@@ -9,8 +9,13 @@ public class DoNothingFilter implements PixelFilter {
     }
 
     @Override
-    public void drawOverlay(PApplet window) {
+    public void drawOverlay(PApplet window, DImage original, DImage filtered) {
         window.fill(255, 0, 0);
+        window.ellipse(original.getWidth(), original.getHeight(), 10, 10);
+
+        window.fill(0, 255, 0);
         window.ellipse(0, 0, 10, 10);
     }
+
 }
+

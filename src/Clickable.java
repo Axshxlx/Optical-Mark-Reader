5e66d49c762ee11084fb4973d3b4435987ebf013
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2021-01-06 20:57:06 -0800

    Add comments

--- src/Clickable.java
+++ src/Clickable.java
@@ -1,4 +1,11 @@
 public interface Clickable {
+    /***
+     * Action you wish to perform when the mouse is clicked.  For example, store the color of the pixel where
+     * the mouse was clicked to set a threshold or a target color.
+     * @param mouseX the x coordiante where the mouse was clicked ( with (0, 0) the upper left corner of your image )
+     * @param mouseY
+     * @param img
+     */
     public void mouseClicked(int mouseX, int mouseY, DImage img);
     public void keyPressed(char key);
 }
--- src/PixelFilter.java
+++ src/PixelFilter.java
@@ -1,6 +1,20 @@
 import processing.core.PApplet;
 
 public interface PixelFilter {
+    /***
+     * apply a filter to an input image and return the filtered image as output
+     * @param img the image to filter
+     * @return the filtered image
+     */
     public DImage processImage(DImage img);
+
+    /***
+     * A method to allow you to draw on top of the filtered image in the display.
+     * This method is run after displaying the filtered image.  You can use
+     * processing commands such as rect(...), line(...), ellipse(...) etc. to draw.
+     * @param window the PApplet window your filtered image is displayed in
+     * @param original the original unfiltered image (in case you want to query pixel values)
+     * @param filtered the output from your processImage method
+     */
     public void drawOverlay(PApplet window, DImage original, DImage filtered);
 }

2bddc787057d8ebf71e389b28ae8bb46f6ce2c9c
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2021-01-15 07:21:07 -0800

    Add hello world for color filters

---
+++ src/BasicColorFilter.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class BasicColorFilter implements PixelFilter {
+
+    @Override
+    public DImage processImage(DImage img) {
+        short[][] red = img.getRedChannel();
+        short[][] green = img.getGreenChannel();
+        short[][] blue = img.getBlueChannel();
+
+        // Do stuff with color channels here
+
+        img.setColorChannels(red, green, blue);
+        return img;
+    }
+
+    @Override
+    public void drawOverlay(PApplet window, DImage original, DImage filtered) {
+
+    }
+
+}
+

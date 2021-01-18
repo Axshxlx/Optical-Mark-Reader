86ce150c6c8f277af80a0e8e4edacb9f17a6f2f6
highjeans <highjeans11@gmail.com>
2021-01-18 09:55:05 -0800

    Added Porcessing 4.0 alpha 3 Video library files to library

--- src/RunMe.java
+++ src/RunMe.java
@@ -1,9 +1,9 @@
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
-        DisplayWindow.showFor("images/6.jpg");
+        //DisplayWindow.showFor("videos/sample1.mp4");
 
         // --== Determine your input interactively with menus ==--
-        //DisplayWindow.getInputInteractively();
+        DisplayWindow.getInputInteractively();
     }
 }

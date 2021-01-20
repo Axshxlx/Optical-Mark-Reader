db24065fd3de8736678a735fcbbdac0948b36581
highjeans <highjeans11@gmail.com>
2021-01-20 09:24:48 -0800

    Got Video playback working and removed unnecessary files

--- src/RunMe.java
+++ src/RunMe.java
@@ -1,9 +1,9 @@
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
-        //DisplayWindow.showFor("videos/sample1.mp4");
+        DisplayWindow.showFor("images/6.jpg");
 
         // --== Determine your input interactively with menus ==--
-        DisplayWindow.getInputInteractively();
+        //DisplayWindow.getInputInteractively();
     }
 }

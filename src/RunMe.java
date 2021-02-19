5a85ba97099070333b42b269f956e583b8a8ae5a
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2021-02-19 11:11:16 -0800

    fix runme to display image by default

--- src/RunMe.java
+++ src/RunMe.java
@@ -1,7 +1,7 @@
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
-        DisplayWindow.showFor("videos/sample1.mp4");
+        DisplayWindow.showFor("images/6.jpg");
 
         // --== Determine your input interactively with menus ==--
         //DisplayWindow.getInputInteractively();

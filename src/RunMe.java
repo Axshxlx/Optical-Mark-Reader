8ed55a5b839e67c98473654a92e9d7bcc76da90f
ddobervich <david.dobervich@gmail.com>
2022-01-17 09:00:34 -0800

    Add constructor parameters for changing display dimensions

--- src/RunMe.java
+++ src/RunMe.java
@@ -1,9 +1,11 @@
+import core.DisplayWindow;
+
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
-        DisplayWindow.showFor("images/6.jpg");
+        DisplayWindow.showFor("images/6.jpg", 800, 600);
 
         // --== Determine your input interactively with menus ==--
-        //DisplayWindow.getInputInteractively();
+        //DisplayWindow.getInputInteractively(800,600);
     }
 }

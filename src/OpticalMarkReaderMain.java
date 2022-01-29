8be6550650155266b7183d7822f32e95c5917953
ddobervich <david.dobervich@gmail.com>
2022-01-28 16:01:49 -0800

    create template main method

---
+++ src/OpticalMarkReaderMain.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.io.File;
+
+public class OpticalMarkReaderMain {
+    public static void main(String[] args) {
+        String pathToPdf = fileChooser();
+        System.out.println("Loading pdf at " + pathToPdf);
+
+        /*
+        Your code here to...
+        (1).  Load the pdf
+        (2).  Loop over its pages
+        (3).  Create a DImage from each page and process its pixels
+        (4).  Output 2 csv files
+         */
+
+    }
+
+    private static String fileChooser() {
+        String userDirLocation = System.getProperty("user.dir");
+        File userDir = new File(userDirLocation);
+        JFileChooser fc = new JFileChooser(userDir);
+        int returnVal = fc.showOpenDialog(null);
+        File file = fc.getSelectedFile();
+        return file.getAbsolutePath();
+    }
+}
